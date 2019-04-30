05e45df8b8657076c63fe25e3436c9dccfa45297
coderedm0nztR <aaronalnutt1@gmail.com>
2019-04-30 11:44:58 -0400

    Add files via upload

--- project1.js
+++ project1.js
@@ -16,31 +16,23 @@ function init(){
 			var dataset1 = [];
 
 			for (var i = 0; i < 25; i++) {           //Loop 25 times
-			    var x = (Math.random() * 100).toFixed(2);  //New random number (0-30)
+			    var x = (Math.random() * 100).toFixed(2);  //New random number (0-100)
 			   	var y = (Math.random() * 100).toFixed(2);
 			   	//var s = Math.random() * 50;
 			   	var newNumber = [x, y];
-			    dataset1.push(newNumber);             //Add new number to array
+			    dataset1.push(newNumber);             //Add new numbers to array
 			}
 
 			var dataset2 = [];
+
 			for (var i = 0; i < 25; i++) {           //Loop 25 times
-			    var x = (Math.random() * 100).toFixed(2);  //New random number (0-30)
+			    var x = (Math.random() * 100).toFixed(2);  //New random number (0-100)
 			   	var y = (Math.random() * 100).toFixed(2);
 			   	//var s = Math.random() * 50;
 			   	var newNumber = [x, y];
-			    dataset2.push(newNumber);             //Add new number to array
+			    dataset2.push(newNumber);             //Add new numbers to array
 			}
-			console.log(dataset2);
-			
-			var dataset = [
-							[5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-							[410, 12], [475, 44], [25, 67], [85, 21], [220, 88],
-							[600, 150]
-						  ];
-
-			
-
+		
 			var maxX1 = d3.max(dataset1, function(d) {console.log(d[0]); return d[0]; });
 			var maxX2 = d3.max(dataset2, function(d) {console.log(d[0]); return d[0]; });
 			var maxX = maxX1 > maxX2 ? maxX1 : maxX2;
@@ -52,21 +44,28 @@ function init(){
 			var maxS1 = d3.max(dataset1, function(d) {console.log(d[0]); return d[0]; });
 			var maxS2 = d3.max(dataset2, function(d) {console.log(d[0]); return d[0]; });
 			var maxS = maxY1 > maxX2 ? maxY1 : maxY2;
+
+
+		    var mean1 = d3.mean(dataset1, function(d) {console.log(d[0]); return d[0]; });
+		    var mean2 = d3.mean(dataset1, function(d) {console.log(d[0]); return d[0]; });
+
+		    var mean = mean1 + mean2 / 2;
+
 			console.log(maxX + ', ' + maxY);
 
 			//Create scale functions
 			var xScale = d3.scale.linear()
-								 .domain([0, maxX])
+								 .domain([0, 100])
 								 .range([padding, w - padding * 2]);
 
 
 
 			var yScale = d3.scale.linear()
-								 .domain([0, maxY])
+								 .domain([0, 100])
 								 .range([h - padding, padding]);
 
 			var sizeScale = d3.scale.linear()
-								 .domain([0, maxS])
+								 .domain([0, 100])
 								 .range([5, 50]);
 
 			//Define X axis
@@ -88,76 +87,50 @@ function init(){
 						.attr("height", h);
 
 			//Create circles
-			var te =  d3.svg.symbol().type('triangle-down');
-			te.type = 'triangle-down';
-			svg.selectAll(".dataset2ircle")
+			//var te =  d3.svg.symbol().type('triangle-down');
+			//te.type = 'triangle-down';
+			svg.selectAll(".dataset2circle")
 			   .data(dataset2)
 			   .enter()
 			   .append("path")
 			   .attr('class', 'dataset2circle')
-			   .attr('fill', 'red')
+			   .attr('fill', 'white')
+			   .attr('stroke', function(d){
+			   		if (d[0] > mean){
+			   			return 'red';
+			   		}
+			   		else{
+			   			return 'green';
+			   		}
+			   })
 			   .attr("transform", function(d) {
 			  		 return "translate(" + xScale(d[0]) + "," + yScale(d[1]) + ")"; }
 			  	)
     			.attr("d", d3.svg.symbol().type('triangle-down')
-					.size(function(d){ return sizeScale(d[0]); }))
+				.size(function(d){ return sizeScale(d[0]); }))
 			  ;
 
-			//Create labels
-			/*
-			svg.selectAll(".dataset2label")
-			   .data(dataset2)
-			   .enter()
-			   .append("text")
-			   .attr('class', 'dataset2label')
-			   .text(function(d) {
-			   		return d[0][0] + "," + d[0][1];
-			   })
-			   .attr("x", function(d) {
-			   		return xScale(d[0][0]);
-			   })
-			   .attr("y", function(d) {
-			   		return yScale(d[0][1]);
-			   })
-			   .attr("font-family", "sans-serif")
-			   .attr("font-size", "11px")
-			   .attr("fill", "red");
-*/
-
+			
 			//Create circles
 			svg.selectAll(".dataset1circle")
 			   .data(dataset1)
 			   .enter()
 			   .append("path")
 			   .attr('class', 'dataset1circle')
-			    .attr("transform", function(d) {
-			  		 return "translate(" + xScale(d[0]) + "," + yScale(d[1]) + ")"; }
-			  	).attr('fill', 'green')
-    			.attr("d", d3.svg.symbol().type('circle')
-					.size(function(d){ return sizeScale(d[0]); }))
-			  ;
+			   .attr("transform", function(d) {
+			  		 return "translate(" + xScale(d[0]) + "," + yScale(d[1]) + ")"; })
+			   .attr('fill', 'white')
+			   .attr('stroke', function(d){
+			   		if (d[0] > mean){
+			   			return 'red';
+			   		}
+			   		else{
+			   			return 'green';
+			   		}
+			   })
+    		   .attr("d", d3.svg.symbol().type('circle')
+			   .size(function(d){ return sizeScale(d[0]); }));
 
-
-			//Create labels
-			/*
-			svg.selectAll(".dataset1label")
-			   .data(dataset1)
-			   .enter()
-			   .append("text")
-			   .attr('class', 'dataset1label')
-			   .text(function(d) {
-			   		return d[0][0] + "," + d[0][1];
-			   })
-			   .attr("x", function(d) {
-			   		return xScale(d[0][0]);
-			   })
-			   .attr("y", function(d) {
-			   		return yScale(d[0][1]);
-			   })
-			   .attr("font-family", "sans-serif")
-			   .attr("font-size", "11px")
-			   .attr("fill", "red");*/
-			
 			//Create X axis
 			svg.append("g")
 				.attr("class", "axis")
@@ -169,6 +142,12 @@ function init(){
 				.attr("class", "axis")
 				.attr("transform", "translate(" + padding + ",0)")
 				.call(yAxis);
+
+
+			svg.append('text')
+				.attr('x', '240')
+				.attr('y', '470')
+				.text('Example Scatter Plot');
 }
 
 function init1 () {
